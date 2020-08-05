82d646d69b45d2c86df5600fa10f73ae8afb8211
Filip Pajic <fica.pajic@gmail.com>
2020-08-05 21:17:05 +0200

    Implemented a config example for the pallet contract

---
+++ src/pallets/configs/pallet_contracts.config.ts
@@ -0,0 +1,131 @@
+import {
+  IPalletConfig,
+  ESupportedPallets,
+  ESubstrateVersion,
+  EPalletCategories,
+  ECommonAuthors,
+  EPalletModuleParts,
+  defaultGitRepo
+} from '../pallets.types';
+
+enum EPalletContractsTraits {
+  Time = 'Time',
+  Randomness = 'Randomness',
+  Currency = 'Currency',
+  Event = 'Event',
+  DetermineContractAddress = 'DetermineContractAddress',
+  TrieIdGenerator = 'TrieIdGenerator',
+  RentPayment = 'RentPayment',
+  SignedClaimHandicap = 'SignedClaimHandicap',
+  TombstoneDeposit = 'TombstoneDeposit',
+  StorageSizeOffset = 'StorageSizeOffset',
+  RentByteFee = 'RentByteFee',
+  RentDepositOffset = 'RentDepositOffset',
+  SurchargeReward = 'SurchargeReward',
+  MaxDepth = 'MaxDepth',
+  MaxValueSize = 'MaxValueSize',
+  WeightPrice = 'WeightPrice'
+}
+
+enum EPalletContractsGenesisFields {
+  current_schedule = 'current_schedule'
+}
+
+const palletDescription = [
+  'FRAME pallet for WASM contracts.',
+  'The Contract module provides functionality for the runtime to deploy and execute WebAssembly smart-contracts.',
+  'This module extends accounts based on the `Currency` trait to have smart-contract functionality. It can be used with other modules that implement accounts based on `Currency`. These "smart-contract accounts" have the ability to instantiate smart-contracts and make calls to other contract and non-contract accounts.',
+  'The smart-contract code is stored once in a `code_cache`, and later retrievable'
+].join('\n');
+
+const PalletContractsConfig: IPalletConfig<EPalletContractsTraits, EPalletContractsGenesisFields> = {
+  name: ESupportedPallets.PALLET_CONTRACT,
+  metadata: {
+    compatibility: ESubstrateVersion.TWO,
+    size: 75500,
+    updated: 1596018720,
+    license: 'Apache-2.0',
+    authors: [ECommonAuthors.PARITY_TECHNOLOGIES],
+    categories: [EPalletCategories.SMART_CONTRACTS],
+    description: palletDescription
+  },
+  dependencies: {
+    pallet: {
+      alias: 'contracts',
+      gitRepo: defaultGitRepo,
+      package: 'pallet-contracts',
+      tag: 'v2.0.0-rc5',
+      version: '2.0.0-rc5',
+      defaultFeatures: false
+    },
+    additionalPallets: [ESupportedPallets.PALLET_BALANCE],
+    additionalDeps: [
+      {
+        alias: 'contracts-primitives',
+        gitRepo: defaultGitRepo,
+        package: 'pallet-contracts-primitives',
+        tag: '2.0.0-rc5',
+        version: '2.0.0-rc',
+        defaultFeatures: false
+      }
+    ]
+  },
+  runtime: {
+    constructRuntime: {
+      modules: [
+        EPalletModuleParts.MODULE,
+        EPalletModuleParts.CALL,
+        EPalletModuleParts.CONFIG,
+        EPalletModuleParts.STORAGE,
+        EPalletModuleParts.EVENT
+      ],
+      generic: {
+        [EPalletModuleParts.EVENT]: true,
+      }
+    },
+    palletTraits: {
+      Time: 'Timestamp',
+      Currency: 'Balances',
+      Randomness: 'RandomnessCollectiveFlip',
+      Event: 'Event',
+      DetermineContractAddress: 'contracts::SimpleAddressDeterminer<Runtime>',
+      TrieIdGenerator: 'contracts::TrieIdFromParentCounter<Runtime>',
+      RentPayment: '()',
+      SignedClaimHandicap: 'contracts::DefaultSignedClaimHandicap',
+      TombstoneDeposit: {
+        constantType: 'Balance',
+        value: "16_000_000_000"
+      },
+      StorageSizeOffset: 'contracts::DefaultStorageSizeOffset',
+      RentByteFee: {
+        constantType: 'Balance',
+        value: "4_000_000_000"
+      },
+      RentDepositOffset: {
+        constantType: 'Balance',
+        value: "1_000_000_000_000"
+      },
+      SurchargeReward: {
+        constantType: 'Balance',
+        value: "150_000_000_000"
+      },
+      MaxDepth: 'contracts::DefaultMaxDepth',
+      MaxValueSize: 'contracts::DefaultMaxValueSize',
+      WeightPrice: 'transaction_payment::Module<Self>'
+    },
+    genesisConfig: {
+      configStructName: 'ContractsConfig',
+      structFields: {
+        current_schedule: "ContractsSchedule {enable_println,..Default::default()}"
+      }
+    },
+    additionalRuntimeLibCode: [
+      'pub use contracts::Schedule as ContractsSchedule;'
+    ],
+    additionalChainSpecCode: [
+      'use node_template_runtime::{ContractsConfig, ContractsSchedule};'
+    ]
+  }
+};
+
+export default PalletContractsConfig;
